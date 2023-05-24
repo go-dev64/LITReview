03b5fcd9dccb559745f5a844bdbf90063059b303
Guillaume <guillaume.oblin@greta-cfa-aquitaine.academy>
2023-05-24 10:10:42 +0200

    creation user et superuser

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    SUBSCRIBER = "SUBSCRIBER"
+
+    profil_photo = models.ImageField(verbose_name="Photo de profil")
