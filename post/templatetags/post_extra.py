392ab74f95d6915a3b97019c0f16bf652da28a21
Guillaume <guillaume.oblin@greta-cfa-aquitaine.academy>
2023-06-05 18:17:24 +0200

    creation des partials card

--- post/templatetags/post_extra.py
+++ post/templatetags/post_extra.py
@@ -13,18 +13,18 @@ def model_type(value):
     return type(value).__name__
 
 
-@register.simple_tag(takes_context=True)
-def get_poster_display(context, user):
-    if user == context["user"]:
-        return "vous"
-    return user.username
-
-
 @register.filter
 def get_posted_at_display(posted_at):
     seconds_ago = (timezone.now() - posted_at).total_seconds()
     if seconds_ago <= HOUR:
-        return f"Publié il y a {int(seconds_ago // MINUTE)} minutes."
+        return f"Publié il y a un instant."
     elif seconds_ago <= DAY:
         return f"Publié il y a {int(seconds_ago // HOUR)} heures."
     return f'Publié le {posted_at.strftime("%d %b %y à %Hh%M")}'
+
+
+@register.simple_tag(takes_context=True)
+def get_poster_display(context, user):
+    if user == context["user"]:
+        return "Vous avez"
+    return f"{user.username} a"
