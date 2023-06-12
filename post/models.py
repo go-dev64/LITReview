e6b6f99b504550429a23815ae919769f9768659c
Guillaume <guillaume.oblin@greta-cfa-aquitaine.academy>
2023-06-12 16:24:19 +0200

    ajout image par default ticket

--- post/models.py
+++ post/models.py
@@ -27,7 +27,12 @@ class Ticket(models.Model):
     title = models.CharField(max_length=128)
     description = models.TextField(max_length=2048, blank=True)
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    image = models.ForeignKey(Photo, null=True, on_delete=models.SET_NULL, blank=True)
+    image = models.ForeignKey(
+        Photo,
+        null=True,
+        on_delete=models.SET_NULL,
+        blank=True,
+    )
     time_created = models.DateTimeField(auto_now_add=True)
 
 
